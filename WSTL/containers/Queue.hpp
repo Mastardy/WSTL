004701d64f167160aa1ab54f459865170706e2a3
André <andremgmonteiro01@gmail.com>
2023-07-02 17:44:02 +0100

    Commented and Refactored Stack and Queue

--- WSTL/containers/Queue.hpp
+++ WSTL/containers/Queue.hpp
@@ -7,16 +7,42 @@ namespace WSTL
     class Queue
     {
     protected:
-        List<T> container;
+        List<T> container; // TODO: Replace List<T> with a Vector<T>
 
     public:
+        /**
+         * \brief Default Constructor
+         */
         Queue() : container() { }
 
+        /**
+         * \brief Copy Constructor
+         */
+        Queue(const Queue<T>& other) : container(other.container) { }
+
+        /**
+         * \brief Copy Assignment
+         */
+        Queue<T>& operator=(const Queue<T>& other)
+        {
+            if(other == *this) return *this;
+
+            container = other.container;
+
+            return *this;
+        }
+        
+        /**
+         * \brief Adds a value to the back of the queue
+         */
         void Push(const T& value)
         {
             container.PushBack(value);
         }
 
+        /**
+         * \brief Removes and returns the value from the front of the queue
+         */
         T Pop()
         {
             T value = container.Front();
@@ -24,19 +50,33 @@ namespace WSTL
             return value;
         }
 
+        /**
+         * \brief Returns the value from the front of the queue
+         */
         T& Front()
         {
             return container.Front();
         }
-        
+
+        /**
+         * \brief Returns if the queue is empty
+         */
         bool IsEmpty() const noexcept
         {
             return container.IsEmpty();
         }
-        
+
+        /**
+         * \brief Returns the size of the queue
+         */
         size_t Size() const noexcept
         {
             return container.Size();
         }
+
+        // Delete move operations
+        Queue<T>& operator=(Queue<T>&& other) = delete;
+        Queue(Queue<T>&& other) = delete;
+        ~Queue() = default;
     };
 }
--- WSTL/containers/Stack.hpp
+++ WSTL/containers/Stack.hpp
@@ -3,20 +3,41 @@
 
 namespace WSTL
 {
-    template<typename T> // TODO: Replace List<T> with a Vector<T>
+    template<typename T>
     class Stack
     {
     protected:
-        List<T> container;
+        List<T> container; // TODO: Replace List<T> with a Vector<T>
         
     public:
+        /**
+         * \brief Default Constructor
+         */
         Stack() : container() { }
 
+        /**
+         * \brief Copy Constructor
+         */
         void Push(const T& value)
         {
             container.PushBack(value);
         }
 
+        /**
+         * \brief Copy Assignment
+         */
+        Stack<T>& operator=(const Stack<T>& other)
+        {
+            if(this == &other) return *this;
+
+            container = other.container;
+
+            return *this;
+        }
+
+        /**
+         * \brief Removes and returns the value from the top of the stack
+         */
         T Pop()
         {
             T value = container.Back();
@@ -24,16 +45,25 @@ namespace WSTL
             return value;
         }
 
+        /**
+         * \brief Returns the value from the top of the stack
+         */
         T Top()
         {
             return container.Back();
         }
-        
+
+        /**
+         * \brief Returns if the stack is empty
+         */
         bool IsEmpty() const noexcept
         {
             return container.IsEmpty();
         }
-        
+
+        /**
+         * \brief Returns the size of the stack
+         */
         size_t Size() const noexcept
         {
             return container.Size();
