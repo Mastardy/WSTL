6b6184992213d3e14c7aec7f34b5c627b27ffaca
André Monteiro <andremgmonteiro01@gmail.com>
2023-09-04 01:55:47 +0100

    Implemented Binary Tree

--- Sandbox/Sandbox.cpp
+++ Sandbox/Sandbox.cpp
@@ -1,5 +1,6 @@
-#include <iostream>
 #include <WSTL.hpp>
+#include <chrono>
+#include <iostream>
 
 #include "containers/BTree.hpp"
 
@@ -8,24 +9,17 @@ using namespace WSTL;
 int main()
 {
     BTree<int> tree;
-    constexpr int max = 1000;
-    auto start = std::chrono::high_resolution_clock::now();
-    for(int i = 0; i < max; ++i)
+
+    for(int i = 0; i < 100; ++i)
     {
-        tree.Insert(i);
+        tree.Insert(i); 
     }
-    auto end = std::chrono::high_resolution_clock::now();
-    std::cout << "Insert took " << std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() << "ns" << std::endl;
 
-    start = std::chrono::high_resolution_clock::now();
-    tree.Search(max - 1);
-    end = std::chrono::high_resolution_clock::now();
-    std::cout << "Search took " << std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() << "ns" << std::endl;
+    std::cout << tree.Size() << std::endl;
+    tree.Delete(42);
+    std::cout << tree.Size() << std::endl;
+    std::cout << tree.Size() << std::endl;
 
-    start = std::chrono::high_resolution_clock::now();
-    tree.InternalSearch(tree.GetRoot(), max - 1);
-    end = std::chrono::high_resolution_clock::now();
-    std::cout << "Internal Search took " << std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() << "ns" << std::endl;
     
     return 0;
 }
--- WSTL/containers/BTree.hpp
+++ WSTL/containers/BTree.hpp
@@ -13,22 +13,30 @@ namespace WSTL
         BTreeNode(T data = T()) : data(data), pLeft(nullptr), pRight(nullptr) {}
     };
 
-    class BTreeIterator
+    template<typename T>
+    struct BTreeIterator
     {
-        
+        typedef BTreeNode<T> Node;
+
+        Node* pNode = nullptr;
+        Node* pParent = nullptr;
+        bool isLeft = false;
+
+        auto operator<=>(const BTreeIterator&) const = default;
     };
 
     template<typename T>
     class BTree
     {
         typedef BTreeNode<T> Node;
+        typedef BTreeIterator<T> Iterator;
         
     public:
         /**
          * \brief Default Constructor
          */
         BTree() : pRoot(nullptr) { }
-
+        
         /**
          * \brief Insert a value into the tree
          */
@@ -40,33 +48,8 @@ namespace WSTL
                 return;
             }
             
-            Queue<Node*> queue;
-            queue.Push(pRoot);
-
-            while(!queue.IsEmpty())
-            {
-                Node* pTemp = queue.Pop();
-
-                if(pTemp->pLeft == nullptr)
-                {
-                    pTemp->pLeft = new Node(value);
-                    return;
-                }
-                else
-                {
-                    queue.Push(pTemp->pLeft);
-                }
-
-                if(pTemp->pRight == nullptr)
-                {
-                    pTemp->pRight = new Node(value);
-                    return;
-                }
-                else
-                {
-                    queue.Push(pTemp->pRight);
-                }
-            }
+            Node* pNew = InternalGetNewNode();
+            pNew->data = value;
         }
 
         /**
@@ -75,39 +58,73 @@ namespace WSTL
         void Delete(T value)
         {
             Node* pSearch = Search(value);
+            if(pSearch == nullptr) return;
+            Iterator itLast = InternalGetLast(pRoot, Height());
+            if(itLast.pNode == nullptr) return;
 
-            
-        }
+            std::swap(pSearch->data, itLast.pNode->data);
 
-        /**
-         * \brief 
-         */
-        void DeleteAll(T value)
-        {
-            
+            if(itLast.isLeft)
+            {
+                itLast.pParent->pLeft = nullptr;
+                Free(&(itLast.pNode));
+            }
+            else
+            {
+                itLast.pParent->pRight = nullptr;
+                Free(&(itLast.pNode));
+            }
         }
         
         /**
-         * \brief 
+         * \brief Search for a value in the tree
          */
-        Node* Search(T value)
+        inline Node* Search(T value)
         {
-            Queue<Node*> queue;
-            queue.Push(pRoot);
-
-            while(!queue.IsEmpty())
-            {
-                Node* pTemp = queue.Front();
-                queue.Pop();
-
-                if(pTemp->data == value) return pTemp;
-                if(pTemp->pLeft != nullptr) queue.Push(pTemp->pLeft);
-                if(pTemp->pRight != nullptr) queue.Push(pTemp->pRight);
-            }
-
-            return nullptr;
+            return InternalSearch(pRoot, value);
         }
-    
+
+        /**
+         * \brief Get the last node in the tree
+         */
+        inline Node* GetLast()
+        {
+            return InternalGetLast(pRoot, Height()).pNode;
+        }
+
+        /**
+         * \brief Get the height of the tree
+         */
+        inline int Height()
+        {
+            return InternalHeight(pRoot);
+        }
+
+        Size Size()
+        {
+            return InternalSize(pRoot);
+        }
+
+        bool IsEmpty()
+        {
+            return pRoot == nullptr;
+        }
+
+        void Clear()
+        {
+            InternalClear(pRoot);
+            pRoot = nullptr;
+        }
+
+        /**
+         * \brief Get the root node of the tree
+         */
+        Node* GetRoot() { return pRoot; }
+        
+    protected:
+        /**
+         * \brief Internal search function
+         */
         Node* InternalSearch(Node* pTemp, T value)
         {
             if(pTemp == nullptr) return nullptr;
@@ -120,7 +137,116 @@ namespace WSTL
             return nullptr;
         }
 
-        Node* GetRoot() { return pRoot; }
+        /**
+         * \brief Internal function to get the last node in the tree
+         */
+        Iterator InternalGetLast(Node* pTemp, int height)
+        {
+            if(pTemp == nullptr) return { };
+            if(height == 1)
+            {
+                if(pTemp->pRight != nullptr)
+                {
+                    return { pTemp->pRight, pTemp, false };
+                }
+                else if(pTemp->pLeft != nullptr)
+                {
+                    return { pTemp->pLeft, pTemp, true };
+                }
+
+                return { };
+            }
+
+            auto pRight = InternalGetLast(pTemp->pRight, height - 1);
+            if(pRight.pNode != nullptr) return pRight;
+            auto pLeft = InternalGetLast(pTemp->pLeft, height - 1);
+            if(pLeft.pNode != nullptr) return pLeft;
+            
+            return { };
+        }
+
+        /**
+         * \brief Internal function to get a new node in the tree
+         */
+        Node* InternalGetNewNode()
+        {
+            const int height = Height();
+            
+            Node* pTemp = pRoot;
+            int i = 0;
+            while(pTemp->pRight != nullptr)
+            {
+                pTemp = pTemp->pRight;
+                ++i;
+            }
+            if(height == i)
+            {
+                pTemp = pRoot;
+                while(pTemp->pLeft != nullptr) pTemp = pTemp->pLeft;
+                pTemp->pLeft = new Node();
+                return pTemp->pLeft;
+            }
+
+            return InternalGetNewNodeRecursive(pRoot, height);
+        }
+
+        /**
+         * \brief Internal function to get a new node in the tree
+         */
+        Node* InternalGetNewNodeRecursive(Node* pTemp, int height)
+        {
+            if(pTemp == nullptr) return nullptr;
+            
+            if(height == 1 && pTemp != nullptr)
+            {
+                if(pTemp->pLeft == nullptr)
+                {
+                    pTemp->pLeft = new Node();
+                    return pTemp->pLeft;
+                }
+                else if(pTemp->pRight == nullptr)
+                {
+                    pTemp->pRight = new Node();
+                    return pTemp->pRight;
+                }
+            }
+
+            Node* pLeft = InternalGetNewNodeRecursive(pTemp->pLeft, height - 1);
+            if(pLeft != nullptr) return pLeft;
+            Node* pRight = InternalGetNewNodeRecursive(pTemp->pRight, height - 1);
+            if(pRight != nullptr) return pRight;
+
+            return nullptr;
+        }
+
+        /**
+         * \brief Internal function to get the height of the tree
+         */
+        int InternalHeight(Node* pTemp)
+        {
+            if(pTemp->pLeft == nullptr) return 0;
+            return 1 + InternalHeight(pTemp->pLeft);
+        }
+
+        /**
+         * \brief Internal function to get the size of the tree
+         */
+        ::Size InternalSize(Node* pTemp)
+        {
+            if(pTemp == nullptr) return 0;
+            return 1 + InternalSize(pTemp->pLeft) + InternalSize(pTemp->pRight);
+        }
+
+        /**
+         * \brief Internal function to clear the tree
+         */
+        void InternalClear(Node* pTemp)
+        {
+            if(pTemp == nullptr) return;
+            if(pTemp->pLeft != nullptr) InternalClear(pTemp->pLeft);
+            if(pTemp->pRight != nullptr) InternalClear(pTemp->pRight);
+            Free(&pTemp);
+        }
         
     private:
         Node* pRoot;
