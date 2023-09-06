24b01136a7c69c8bda126133a8f8e534cfce1322
André Monteiro <andremgmonteiro01@gmail.com>
2023-09-06 16:55:21 +0100

    Started implementing Map Test

--- Sandbox/Sandbox.cpp
+++ Sandbox/Sandbox.cpp
@@ -5,30 +5,15 @@
 
 int main()
 {
-    WSTL::Map<unsigned int, std::string> results;
+    using namespace WSTL;
 
-    results[1] = "Teste";
-    results[2] = "Teste2";
-    results[3] = "Teste3";
-    results[4] = "Teste4";
-    results[5] = "Teste5";
-    results[6] = "Teste6";
-    results[7] = "Teste7";
-    results[8] = "Teste8";
-    results[9] = "Teste9";
-    
-    std::cout << results.Size() << std::endl;
-    auto keys = results.GetKeys();
-    for(const auto& key : keys)
-    {
-        std::cout << key << std::endl;
-    }
+    Map<int, char> a;
+    a.Insert(1, 'a');
+    a.Insert(2, 'b');
+    a.Insert(3, 'c');
 
-    auto values = results.GetValues();
-    for(const auto& value : values)
-    {
-        std::cout << value << std::endl;
-    }
+    Map<int, char> b(a);    
+    Map<int, char> c = a;
     
     return 0;
 }
---
+++ UnitTestProject/MapTest.cpp
@@ -0,0 +1,86 @@
+﻿#include <gtest/gtest.h>
+#include "containers/Map.hpp"
+
+using namespace WSTL;
+
+typedef Map<unsigned int, char> MapType;
+
+TEST(MapTest, Constructor)
+{
+    const MapType a;
+    EXPECT_EQ(a.Size(), 0);
+}
+
+TEST(MapTest, Copy)
+{
+    MapType a;
+    a.Insert(1, 'a');
+    a.Insert(2, 'b');
+    a.Insert(3, 'c');
+    
+    MapType b(a);
+    EXPECT_EQ(b.Size(), 3);
+    EXPECT_EQ(b[1], 'a');
+    EXPECT_EQ(b[2], 'b');
+
+    MapType c = a;
+    
+    EXPECT_EQ(c.Size(), 3);
+    EXPECT_EQ(c[1], 'a');
+    EXPECT_EQ(c[2], 'b');
+}
+
+TEST(MapTest, Move)
+{
+    MapType a;
+    a.Insert(1, 'a');
+    a.Insert(2, 'b');
+    a.Insert(3, 'c');
+    
+    MapType b(std::move(a));
+    EXPECT_EQ(b.Size(), 3);
+    EXPECT_EQ(b[1], 'a');
+    EXPECT_EQ(b[2], 'b');
+
+    MapType c = std::move(b);
+    
+    EXPECT_EQ(c.Size(), 3);
+    EXPECT_EQ(c[1], 'a');
+    EXPECT_EQ(c[2], 'b');
+}
+
+TEST(MapTest, Size)
+{
+    MapType a;
+    EXPECT_EQ(a.Size(), 0);
+    a.Insert(1, 'a');
+    EXPECT_EQ(a.Size(), 1);
+    a.Insert(2, 'b');
+    EXPECT_EQ(a.Size(), 2);
+    a.Insert(3, 'c');
+    EXPECT_EQ(a.Size(), 3);
+    a.Erase(1);
+    EXPECT_EQ(a.Size(), 2);
+    a.Erase(2);
+    EXPECT_EQ(a.Size(), 1);
+    a.Erase(3);
+    EXPECT_EQ(a.Size(), 0);
+}
+
+TEST(MapTest, Insert)
+{
+    MapType a;
+    a.Insert(1, 'a');
+    EXPECT_EQ(a.Size(), 1);
+    EXPECT_EQ(a[1], 'a');
+    a.Insert(2, 'b');
+    EXPECT_EQ(a.Size(), 2);
+    EXPECT_EQ(a[2], 'b');
+    a.Insert(3, 'c');
+    EXPECT_EQ(a.Size(), 3);
+    EXPECT_EQ(a[3], 'c');
+
+    a[4] = 'd';
+    EXPECT_EQ(a.Size(), 4);
+    EXPECT_EQ(a[4], 'd');
+}
